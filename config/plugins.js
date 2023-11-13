1e88c8fda71202f3a634e9469647d17c4acc0d32
gwagsi <gwagsiglenn@gmail.com>
2023-11-13 15:36:48 +0100

    fix email setup

--- config/plugins.js
+++ config/plugins.js
@@ -18,16 +18,22 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    
     email: {
       config: {
-        provider: 'strapi-provider-email-sendinblue',
+        provider: 'nodemailer', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
         providerOptions: {
-          sendinblue_api_key:  'Vy1Gc4MNnwb83kdY' ,
-          sendinblue_default_replyto: "nachass@nchhass.com",
-          sendinblue_default_from: "nachass@nchhass.com",
-          sendinblue_default_from_name: "nachass@nchhass.com",
+          host: "smtp-relay.brevo.com", 
+          port:   587 ,
+          auth: {
+            user:  'gwagsiglenn@gmail.com' ,
+            pass:  'Vy1Gc4MNnwb83kdY' ,
+        },
+        settings: {
+          defaultFrom: 'nachass@nchhass.com',
+          defaultReplyTo: 'nachass@nchhass.com',
+          testAddress: 'nachass@nchhass.com',
         },
       },
     },
-     
-  }) 
+  }}) 
